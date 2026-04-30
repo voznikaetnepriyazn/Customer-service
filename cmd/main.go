d3d3636a9b8c1b6dae788d938354bbd975b86398
Daria Maslova <maslovadaria16.07@gmail.com>
2026-05-01 00:17:33 +0300

    entry point

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,104 @@
 package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/gofiber/fiber/v3"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/joho/godotenv"
+
+	"github.com/voznikaetnepriyazn/Customer-service/internal/config"
+	handlers "github.com/voznikaetnepriyazn/Customer-service/internal/http-server/handlers/customer"
+	"github.com/voznikaetnepriyazn/Customer-service/internal/lib/logger/sl"
+	"github.com/voznikaetnepriyazn/Customer-service/internal/storage"
+	"github.com/voznikaetnepriyazn/Customer-service/internal/storage/postgresql"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		slog.Error(".env file not found", sl.Err(err))
+	}
+
+	cfg := config.MustLoad()
+
+	logger := setUpLogger(cfg.Env)
+
+	if logger == nil {
+		logger = slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		}))
+		slog.SetDefault(logger)
+	} else {
+		slog.SetDefault(logger)
+	}
+
+	logger.Info("starting customer service", slog.String("env", cfg.Env))
+	logger.Debug("debug messages are enabled")
+
+	db, err := postgresql.New(cfg.DB.DSN())
+	if err != nil {
+		slog.Error("failed to connect to database", sl.Err(err))
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	customerService := storage.CustomerService(db)
+
+	app := fiber.New(fiber.Config{
+		AppName:       "Customer Service v1.0",
+		CaseSensitive: true,
+		StrictRouting: true,
+		ServerHeader:  "Fiber",
+	})
+
+	registerRouter(app, logger, customerService)
+
+	addr := cfg.HTTPServer.Address
+	if addr == "" {
+		addr = ":8081"
+	}
+
+	logger.Info("starting server", slog.String("address", addr))
+	if err := app.Listen(addr); err != nil {
+		logger.Error("failed to start server", slog.Any("error", err))
+		os.Exit(1)
+	}
+}
+
+func registerRouter(app *fiber.App, log *slog.Logger, service storage.CustomerService) {
+	api := app.Group("/api/v1")
+
+	api.Post("/customer", handlers.NewAdd(log, service))
+	api.Get("/customer/:id", handlers.NewGetById(log, service))
+	api.Get("/customer", handlers.NewGetAll(log, service))
+	api.Put("/customer/:id/fullName", handlers.NewUpdate(log, service))
+	api.Delete("customer/:id", handlers.NewDelete(log, service))
+	api.Get("/customer/:id", handlers.NewIscustomerCreated(log, service))
+}
+
+func setUpLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+}
--- go.mod
+++ go.mod
@@ -14,6 +14,10 @@ require (
 	github.com/gofiber/utils/v2 v2.0.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.9.2 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.18.4 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -25,6 +29,7 @@ require (
 	github.com/valyala/fasthttp v1.69.0 // indirect
 	golang.org/x/crypto v0.48.0 // indirect
 	golang.org/x/net v0.50.0 // indirect
+	golang.org/x/sync v0.19.0 // indirect
 	golang.org/x/sys v0.41.0 // indirect
 	golang.org/x/text v0.34.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
